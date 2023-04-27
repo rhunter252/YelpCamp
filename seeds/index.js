c4f69733265d926826d5edee328bca1104054f47
Randy Hunter <randyh252@gmail.com>
2023-04-27 00:00:17 -0400

    error handling

--- seeds/index.js
+++ seeds/index.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+import * as dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 
@@ -14,6 +13,24 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
+
+async function seedImg(random10) {
+  try {
+    const res = await axios.get("https://api.pexels.com/v1/search", {
+      headers: {
+        Accept: "application/json",
+        Authorization: PEXELS_API_KEY,
+      },
+      params: {
+        query: "camping",
+      },
+    });
+    // console.log(res.data.photos[random10].src.original);
+    return res.data.photos[random10].src.original;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 const sample = (array) => array[Math.round(Math.random() * array.length)];
 
@@ -25,7 +42,7 @@ const seedDB = async () => {
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
+      image: "await seedImg()",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde commodi ut repellat illo? Praesentium velit cumque non repellat explicabo, animi dolorem possimus, expedita rerum quis aut optio dolore eum sequi!",
       price,
